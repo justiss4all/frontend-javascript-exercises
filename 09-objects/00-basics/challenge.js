a1cee86e9c3809e4f8dd30a337428c1ab1206dcc
Brian <bmjustiss@gmail.com>
2014-10-01 20:26:27 +0000

    09-objects/00-basics solution

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,40 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle, 
+    duration: courseDuration, 
+    students: courseStudents
+  }; 
+  return course; 
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if(newProp in object) {
+    return object; 
+  }
+  else {
+    object[newProp] = newValue; 
+    return object; 
+  } 
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender; 
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  
+  // object containing price table
+  var priceTable = {
+
+    "MacBook Air": 999,  
+    "MacBook Pro": 1299, 
+    "Mac Pro": 2499, 
+    "Apple Sticker": 1
+
+  };
+
+ 
+ result = (priceTable[item] && money >= priceTable[item]) ? true : false;  
+ return result; 
+
+};
